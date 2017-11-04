e44ac99a0f95d3945123a3db2a35dd54b227d5cb
George Paul <george.allen.paul.3@gmail.com>
2017-11-04 16:22:36 -0400

    Add message component and render the list of messages

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Message from './Message'
 
 class MessageList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+    const app = props.db.database().ref('messages')
+    app.on('value', snapshot => {
+      this.getData(snapshot.val())
+    })
+  }
+
+  getData (values) {
+    const messages = Object.keys(values)
+      .map(messageKey => {
+        const cloned = JSON.parse(JSON.stringify(values[messageKey]))
+        cloned.key = messageKey
+        return cloned
+      })
+    this.setState({ messages })
+  }
+
+  renderMessages () {
+    const { messages } = this.state
+    return messages.map(message => <Message {...message} />)
+  }
+
   render () {
     return (
       <div>
-        <p>MessageList Component</p>
+        {this.renderMessages()}
       </div>
     )
   }
