0969eae300a8a246c7d144fe71984d2917b71f0e
George Paul <george.allen.paul.3@gmail.com>
2017-11-04 16:08:16 -0400

    Add message box with firebase connected

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 import Header from './components/Header/Header'
 import MessageList from './components/MessageList/MessageList'
+import MessageBox from './components/MessageBox/MessageBox'
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+    const config = {
+      apiKey: 'AIzaSyBHymkx-sx64eGJFLNmv8Y7nwakdXCNcpQ',
+      authDomain: 'gp3-chat-app.firebaseapp.com',
+      databaseURL: 'https://gp3-chat-app.firebaseio.com',
+      projectId: 'gp3-chat-app',
+      storageBucket: '',
+      messagingSenderId: '111539696639'
+    }
+    firebase.initializeApp(config)
+  }
+
   render () {
     return (
       <div className='App'>
         <Header />
-        <MessageList />
+        <MessageList db={firebase} />
+        <MessageBox db={firebase} />
       </div>
     )
   }
