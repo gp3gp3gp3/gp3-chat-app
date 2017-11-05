65630d0ab5d7770b26e8dcb1d24432984e3ccb40
George Paul <george.allen.paul.3@gmail.com>
2017-11-04 23:30:06 -0400

    fix styling on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,15 @@ import React from 'react'
 import Headroom from 'react-headroom'
 import './Header.css'
 
+const headerStyles = {
+  boxShadow: '1px 1px 1px rgba(0,0,0,0.25)',
+  height: '60px',
+  padding: '20px'
+}
+
 const Header = () => (
   <Headroom
-    style={{background: 'black', height: '60px', padding: '20px'}}
+    style={headerStyles}
     className='App-header'
   >
     <h1 className='App-title'>George's chat app</h1>
