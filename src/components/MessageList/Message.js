bdc7926e029b14e7e905ba1d128598eade6cd028
George Paul <george.allen.paul.3@gmail.com>
2017-11-04 20:18:15 -0400

    Add moment and timestamps to messages

--- src/components/MessageList/Message.js
+++ src/components/MessageList/Message.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import moment from 'moment'
 import './MessageList.css'
 
-const Message = ({ message }) => (
-  <span className='Message'>{message}</span>
-)
+class Message extends PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      messageTime: moment(props.time).fromNow()
+    }
+    this.updateMessageTime = this.updateMessageTime.bind(this)
+  }
+
+  componentDidMount () {
+    this.interval = setInterval(this.updateMessageTime, 3000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
+  updateMessageTime () {
+    const messageTime = moment(this.props.time).fromNow()
+    this.setState({ messageTime })
+  }
+
+  render () {
+    return (
+      <div className='Message-wrapper'>
+        <span className='Message'>{this.props.message}</span>
+        <span className='Message-time'>{this.state.messageTime}</span>
+      </div>
+    )
+  }
+}
 
 export default Message
