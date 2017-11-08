af7ece570cf0add7cad3bdd7971e52e0864fa30e
George Paul <george.allen.paul.3@gmail.com>
2017-11-07 23:26:59 -0500

    Add animation for messages fading in

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { CSSTransitionGroup } from 'react-transition-group'
 import Message from './Message'
 import './MessageList.css'
 
 const MessageList = ({ messages }) => (
   <div className='MessageList'>
-    {messages.map(message => <Message {...message} />)}
+    <CSSTransitionGroup
+      transitionName='message'
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}
+    >
+      {messages.map(message => <Message {...message} />)}
+    </CSSTransitionGroup>
   </div>
 )
 
