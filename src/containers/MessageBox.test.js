6c62234840a5493cc34235872bbdc8e57f7b7b22
George Paul <george.allen.paul.3@gmail.com>
2017-11-14 15:40:05 -0500

    Add container tests for connected components with redux

--- src/containers/MessageBox.test.js
+++ src/containers/MessageBox.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MessageBox from './MessageBox'
+import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
+import MessageBox, { MessageBoxContainer } from './MessageBox'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MessageBox />, div)
+
+it('renders MessageBox with approriate props from store', () => {
+  const mockStore = configureStore([])
+  const store = mockStore({})
+  const messageBoxProps = shallow(<MessageBox store={store} />).find(MessageBoxContainer).props()
+
+  expect(messageBoxProps).toHaveProperty('addMessage')
 })
--- src/containers/MessageList.test.js
+++ src/containers/MessageList.test.js
@@ -1,7 +1,32 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import MessageList from './MessageList'
+import MessageList, { MessageListContainer } from './MessageList'
 
-it('renders without crashing', () => {
-  shallow(<MessageList />)
+const createReduxState = () => {
+  const currentUser = {}
+  const messages = []
+
+  return {
+    currentUser,
+    messages,
+    store: {
+      getState: () => ({
+        auth: { currentUser },
+        messages
+      }),
+      dispatch: () => {},
+      subscribe: () => {},
+    }
+  }
+}
+
+
+it('renders MessageList with approriate props from store', () => {
+  const { store, currentUser, messages } = createReduxState()
+
+  const subject = shallow(<MessageList store={store} />).find(MessageListContainer)
+
+  const actual = subject.prop('currentUser')
+
+  expect(actual).toBe(currentUser)
 })
