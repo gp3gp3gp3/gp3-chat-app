68d0fd2faf4eb1ce439c942563f2e2bdb2c8f5c7
George Paul <george.allen.paul.3@gmail.com>
2017-12-06 23:03:01 -0500

    move app, remove eslint for standard

--- native/__tests__/App.js
+++ native/__tests__/App.js
@@ -1,12 +1,9 @@
 import 'react-native';
-import React from 'react';
-import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import React from 'react';
+import App from '../src/App';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+  renderer.create(<App />);
 });
