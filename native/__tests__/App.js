3aa36e9a28f706878d707e277531640903c10b6d
George Paul <george.allen.paul.3@gmail.com>
2017-12-06 23:09:11 -0500

    Fix linting standard

--- native/__tests__/App.js
+++ native/__tests__/App.js
@@ -1,9 +1,10 @@
-import 'react-native';
+/* eslint-env jest */
+import 'react-native'
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-import React from 'react';
-import App from '../src/App';
+import renderer from 'react-test-renderer'
+import React from 'react'
+import App from '../src/App'
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
--- native/src/App.js
+++ native/src/App.js
@@ -1,26 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
-  View,
-} from 'react-native';
+  View
+} from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-export default class App extends Component<{}> {
-  render() {
+export default class App extends Component {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -33,7 +27,7 @@ export default class App extends Component<{}> {
           {instructions}
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -42,16 +36,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
