f951e1b82e2dccf98a5b0718193bd4fd4329fdf0
George Paul <george.allen.paul.3@gmail.com>
2017-12-07 11:11:14 -0500

    Add snapshot test with enzyme

--- native/__tests__/App.js
+++ native/__tests__/App.js
@@ -1,10 +1,11 @@
 /* eslint-env jest */
 import 'react-native'
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import React from 'react'
 import App from '../src/App'
 
 it('renders correctly', () => {
-  renderer.create(<App />)
+  const wrapper = shallow(<App />)
+  expect(wrapper).toMatchSnapshot()
 })
